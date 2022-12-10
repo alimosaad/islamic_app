f69a6528cdc408fa75b711e0f55d15ee42f0b7f6
ali mosaad <ali.mosaad50@hotmail.com>
2022-12-10 21:38:53 +0200

    add sura to quran tab.

--- lib/screens/home/quran/quran_tab.dart
+++ lib/screens/home/quran/quran_tab.dart
@@ -6,7 +6,7 @@ class QuranTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      width: double.infinity,
       child: Column(
         children: [
           Image.asset("assets/images/Screenshot (1).png"),
