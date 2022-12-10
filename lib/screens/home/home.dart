6b9b9f5ec418a8743ac789db1b8b6652a8cc973b
ali mosaad <ali.mosaad50@hotmail.com>
2022-12-10 21:36:36 +0200

    Create islami app

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/MyThemeData.dart';
+import 'package:islami_app/screens/home/hadeth/hadeth_tab.dart';
+import 'package:islami_app/screens/home/quran/quran_tab.dart';
+import 'package:islami_app/screens/home/radio/radio_tab.dart';
+import 'package:islami_app/screens/home/sebha/sebha_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  static String routeName="home";
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex=0;
+  List<Widget> tabs=[QuranTab(),HadethTab(),SebhaTab(),RadioTab()];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("assets/images/bg3.png"),fit:BoxFit.fill)
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text("Islami",style: TextStyle(color: MyThemeData.accentColor,fontSize: 24),),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(canvasColor: MyThemeData.primaryColor),
+          child: BottomNavigationBar(
+            currentIndex: selectedIndex,
+            onTap: (clickedIndex){
+              selectedIndex=clickedIndex;
+              setState(() {
+
+              });
+            },
+            selectedItemColor: MyThemeData.accentColor,
+            items: const [
+              BottomNavigationBarItem(icon: ImageIcon(AssetImage("assets/images/moshaf_gold.png")),label:"Quran" ),
+              BottomNavigationBarItem(icon: ImageIcon(AssetImage("assets/images/Path 1.png")),label:"Hadeth" ),
+              BottomNavigationBarItem(icon: ImageIcon(AssetImage("assets/images/sebha_blue.png")),label:"Sebha" ),
+              BottomNavigationBarItem(icon: ImageIcon(AssetImage("assets/images/radio_blue.png")),label:"Radio" ),
+
+            ],
+          ),
+        ),
+        body: tabs[selectedIndex],
+
+
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/quran/quran_tab.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class QuranTab extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Column(
+        children: [
+          Image.asset("assets/images/Screenshot (1).png"),
+
+        ],
+      ),
+    );
+  }
+}
