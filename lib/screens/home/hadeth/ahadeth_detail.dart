657dde628a1242ab512959711ebaf4e5325b0f9d
ali mosaad <ali.mosaad50@hotmail.com>
2022-12-14 11:42:39 +0200

    Finish ahadeth detail and content of hadeth.

--- lib/screens/home/hadeth/ahadeth_detail.dart
+++ lib/screens/home/hadeth/ahadeth_detail.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/screens/home/hadeth/hadeth_tab.dart';
+
+import '../../../MyThemeData.dart';
 
 class AhadethDetails extends StatelessWidget {
-
+  static const String routeName = "ahadethDitail";
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var args = ModalRoute.of(context)?.settings.arguments as HadethData;
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/bg3.png"), fit: BoxFit.fill)),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text(
+              args.title,
+              style: TextStyle(color: MyThemeData.BlackColor, fontSize: 24),
+            ),
+            centerTitle: true,
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+          ),
+          body: Container(
+            margin: EdgeInsets.symmetric(vertical: 15,horizontal: 15),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
+                itemCount: args.content.length,
+                itemBuilder: (c, index) {
+              return Text(
+                args.content[index],
+                style: Theme.of(context).textTheme.titleMedium,
+                textAlign: TextAlign.right,
+              );
+            }),
+          )),
+    );
   }
 }
--- lib/screens/home/hadeth/hadeth_tab.dart
+++ lib/screens/home/hadeth/hadeth_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:islami_app/MyThemeData.dart';
+import 'package:islami_app/screens/home/hadeth/ahadeth_detail.dart';
 
 class HadethTab extends StatefulWidget {
   @override
@@ -8,11 +9,11 @@ class HadethTab extends StatefulWidget {
 }
 
 class _HadethTabState extends State<HadethTab> {
-  List<HadethData> Ahadeth=[];
+  List<HadethData> Ahadeth = [];
 
   @override
   Widget build(BuildContext context) {
-    if(Ahadeth.isEmpty) {
+    if (Ahadeth.isEmpty) {
       loadHadeth();
     }
     return Container(
@@ -29,24 +30,40 @@ class _HadethTabState extends State<HadethTab> {
             style: Theme.of(context).textTheme.titleMedium,
           ),
           Divider(
-            color: MyThemeData.primaryColor,
+            color: MyThemeData.OnprimaryColor,
             thickness: 3,
           ),
-          Ahadeth.isEmpty?Center(child: CircularProgressIndicator()):ListView.builder(
-              itemCount: Ahadeth.length,
-              itemBuilder: (c,index){
-
-            return Text(Ahadeth[index].title);
-          })
-
+          Ahadeth.isEmpty
+              ? Center(child: CircularProgressIndicator())
+              : Expanded(
+                  child: ListView.separated(
+                      separatorBuilder: (context, index) => Divider(
+                            indent: 25,
+                            endIndent: 25,
+                          ),
+                      itemCount: Ahadeth.length,
+                      itemBuilder: (c, iindex) {
+                        return InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                  context, AhadethDetails.routeName,
+                                  arguments: Ahadeth[iindex]);
+                            },
+                            child: Text(
+                              Ahadeth[iindex].title,
+                              style: Theme.of(context).textTheme.titleMedium,
+                              textAlign: TextAlign.center,
+                            ));
+                      }),
+                )
         ],
       ),
     );
   }
 
-  void loadHadeth()async {
+  void loadHadeth() async {
     String content = await rootBundle.loadString("assets/files/ahadeth .txt");
-    List<String> allAhadeth = content.split('#');
+    List<String> allAhadeth = content.split('#\r\n');
     for (int i = 0; i < allAhadeth.length; i++) {
       String hadeth1 = allAhadeth[i];
       List<String> hadethLines = hadeth1.split("\n");
@@ -55,13 +72,13 @@ class _HadethTabState extends State<HadethTab> {
       HadethData hadethData = HadethData(title, hadethLines);
       Ahadeth.add(hadethData);
     }
-    setState(() {
-
-    });
+    setState(() {});
   }
 }
-class HadethData{
+
+class HadethData {
   String title;
   List<String> content;
-  HadethData(this.title,this.content);
+
+  HadethData(this.title, this.content);
 }
