843a6f0ebce808b1106386c3bd79c2ebfb1d3d2f
ali mosaad <ali.mosaad50@hotmail.com>
2022-12-18 22:58:36 +0200

    add sharedprefrence.

--- lib/provider/my_provider.dart
+++ lib/provider/my_provider.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:islami_app/provider/sharedprefrence.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class MyProvider extends ChangeNotifier{
   String languageCode='en';
   ThemeMode mode=ThemeMode.light;
+  SharedPreferences? prefs;
+  Future<void> initializeSharedpref() async {
+    prefs= await SharedPreferences.getInstance();
+  }
+
   String getBackground(){
     if (mode==ThemeMode.light){
       return "assets/images/bg3.png";
@@ -27,12 +33,30 @@ class MyProvider extends ChangeNotifier{
       return "assets/images/body_of_seb7a_dark.png";
     }
   }
+  void getThemeAndLang(){
+    String language=Shared.prefs?.getString("lang")??"en";
+    languageCode=language;
+    String theme=Shared.prefs?.getString("theme")??"$mode";
+    if(theme=='$mode'){
+      mode=ThemeMode.light;
+    }
+    else{
+      mode=ThemeMode.dark;
+    }
+
+
+
+
+
+  }
   void changeTheme(ThemeMode modee){
     mode=modee;
+    Shared.prefs?.setString("theme", "$modee");
     notifyListeners();
   }
   void changelang(String lang){
     languageCode=lang;
+    Shared.prefs?.setString("lang", lang);
     notifyListeners();
   }
 }
